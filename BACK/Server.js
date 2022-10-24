bce163bca3e7c1299dc161cc4f82029fb9eb4d30
Carolina Castillo Andrada <carolinacastilloandrad@gmail.com>
2022-10-24 18:26:40 -0300

    no cambios en back

--- BACK/Server.js
+++ BACK/Server.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 
+
+
 // importo conexion mongoDB
 
 const archivoBD = require('./conection')
 
-//creacion de ruta
+//Importacion del archivo de rutas y modelo animal
+
+const animalroute = require('./routes/animal')
+
+
+//Importacion de body parser
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: 'true'}))
+
+
+//creacion de rutas
+
+app.use('/api/animal', animalroute) // localhost3001/api/animal/blabla --> accedo a la ruta que necesito
 
 app.get('/', (req, res) => {
     res.end('wellcome to server eject successful')
