2b51d4489d90bdc0842ec25a9ddc8a234392644f
Carolina Castillo Andrada <carolinacastilloandrad@gmail.com>
2022-10-24 18:26:06 -0300

    funcionalmente terminado falta estilos y extras

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AnimalList from './components/AnimalList';
@@ -12,7 +11,7 @@ function App() {
 
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="#">My CRUD for Basto's test</a>
+          <a className="navbar-brand" href="/">My CRUD for Basto's test</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -29,13 +28,13 @@ function App() {
     </div>
 </nav>
 
-
+{/* //----mis rutas-----// */}
 
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<AnimalList />} exact></Route>
           <Route path='/add' element={<AddAnimals /> }></Route>
-          <Route path='/edit' element={<EditAnimals />}></Route>
+          <Route path='/edit/:idSenasa' element={<EditAnimals />}></Route>
         </Routes>
       </BrowserRouter>
 
--- Client/src/components/AddAnimals.js
+++ Client/src/components/AddAnimals.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import uniqid from 'uniqid';
+import axios from 'axios';
+import Swal from 'sweetalert2'
+
 
 export default function AddAnimals(){
+    //Hooks
+    const [type, setType] = useState('');
+    const [weight, setWeight] = useState('');
+    const [paddling, setPaddling] = useState('');
+    const [device, setDevice] = useState('');
+
+   
 
     function addAnimal(){
-        
+        let animal = {
+            idSenasa: uniqid(),
+            type: type,
+            weight: weight,
+            paddling: paddling,
+            device: device
+        }
+        // console.log(animal, 'estoy en addAnimal front')
+
+        axios.post('api/animal/addanimal', animal)
+        .then(res => {
+            // alert(res.data)
+            Swal.fire('Congratulations!', 'Animal Successfulled Added')
+        })
+        .then(err => {console.log(err)})
     }
 
 
@@ -14,32 +39,38 @@ export default function AddAnimals(){
             </div>
             <div className="row">
                 <div className="col-sm-6 offset-3">
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Name/ID Senasa</label>
-                        <input type="text" className="form-control"></input>
-                    </div>
-
+                    
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Animal Type</label>
-                        <input type="text" className="form-control"></input>
+                        <input type="text" className="form-control" 
+                        value={type} onChange={(e) => {setType(e.target.value)}}
+                        ></input>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Animal Weight</label>
-                        <input type="text" className="form-control"></input>
+                        <input type="text" className="form-control" 
+                        value={weight} onChange={(e) => {setWeight(e.target.value)}}
+                        ></input>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Paddling Name</label>
-                        <input type="text" className="form-control"></input>
+                        <input type="text" className="form-control" 
+                        value={paddling} onChange={(e) => {setPaddling(e.target.value)}}
+                        ></input>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Device Number</label>
-                        <input type="text" className="form-control"></input>
+                        <input type="text" className="form-control" 
+                        value={device} onChange={(e) => {setDevice(e.target.value)}}
+                        ></input>
                     </div>
 
-                    <button onClick={addAnimal} className="btn btn-success">Save</button>
+                    <button 
+                    onClick={addAnimal} 
+                    className="btn btn-success">Save</button>
                 </div>
             </div>
         </div>
